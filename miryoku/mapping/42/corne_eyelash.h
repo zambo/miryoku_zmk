b318ba39627510e44e82b67ab03eb74d5854fd00
zambo <henrique.zambonin@gmail.com>
2025-03-01 19:13:37 -0300

    fix: eyelash_corne config

--- miryoku/mapping/42/corne_eyelash.h
+++ miryoku/mapping/42/corne_eyelash.h
@@ -1,11 +1,11 @@
 // Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#if !defined (MIRYOKU_LAYOUTMAPPING_CORNE)
+#if !defined (MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE)
 
 #define XXX &none
 
-#define MIRYOKU_LAYOUTMAPPING_CORNE( \
+#define MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE( \
      K00, K01, K02, K03, K04,                     K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,                     K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,                     K25, K26, K27, K28, K29, \
@@ -32,4 +32,4 @@ XXX  K20  K21  K22  K23  K24  XXX              XXX          K25  K26  K27  K28  
 
 #endif
 
-#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_CORNE
+#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE
