4ddd2b8c83cbd6c59e67905118a420f7673012c9
zambo <henrique.zambonin@gmail.com>
2025-03-01 20:23:22 -0300

    fix: config

--- miryoku/mapping/42/corne_eyelash.h
+++ miryoku/mapping/42/corne_eyelash.h
@@ -1,17 +1,17 @@
 // Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#if !defined (MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE)
+#if !defined (MIRYOKU_LAYOUTMAPPING_CORNE)
 
 #define XXX &none
 
-#define MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE( \
-     K00, K01, K02, K03, K04,                     K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,                     K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,                     K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,                     K35, K36, K37, N38, N39 \
+#define MIRYOKU_LAYOUTMAPPING_CORNE( \
+     K00, K01, K02, K03, K04,                               K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                               K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                               K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                               K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04                   XXX           K05  K06  K07  K08  K09  XXX \
+XXX  K00  K01  K02  K03  K04                   XXX          K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14              XXX  XXX  XXX     K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24  XXX              XXX          K25  K26  K27  K28  K29  XXX \
                K32  K33  K34                                K35  K36  K37
@@ -32,4 +32,4 @@ XXX  K20  K21  K22  K23  K24  XXX              XXX          K25  K26  K27  K28  
 
 #endif
 
-#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE
+#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_CORNE
