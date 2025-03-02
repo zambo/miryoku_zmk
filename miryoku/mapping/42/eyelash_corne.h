d3e511c5372eea2f12ba892813008ee2ce0e53a1
zambo <henrique.zambonin@gmail.com>
2025-03-02 19:10:46 -0300

    chore: file naming to follow main repo

--- miryoku/mapping/42/eyelash_corne.h
+++ miryoku/mapping/42/eyelash_corne.h
@@ -1,21 +1,20 @@
 // Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-// cspell:ignore MIRYOKU LAYOUTMAPPING KLUDGE TOPROWCOMBOS LEFTPINKIE LEFTINNERINDEX RIGHTINNERINDEX RIGHTPINKIE BOTTOMROWCOMBOS THUMBCOMBOS MOUSEKEYSPR UNDERGLOW KSCAN CTLR
 
-#if !defined (MIRYOKU_LAYOUTMAPPING_CORNE)
+#if !defined (MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE)
 
 #define XXX &none
 
-#define MIRYOKU_LAYOUTMAPPING_CORNE( \
-     K00, K01, K02, K03, K04,                               K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,                               K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,                               K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,                               K35, K36, K37, N38, N39 \
+#define MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE( \
+     K00, K01, K02, K03, K04,                     K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                     K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                     K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                     K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04                   XXX          K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14              XXX  XXX  XXX     K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24  XXX              XXX          K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34                                K35  K36  K37
+XXX  K00  K01  K02  K03  K04            XXX       K05  K06  K07  K08  K09  XXX \
+XXX  K10  K11  K12  K13  K14       XXX  XXX  XXX  K15  K16  K17  K18  K19  XXX \
+XXX  K20  K21  K22  K23  K24  XXX       XXX       K25  K26  K27  K28  K29  XXX \
+               K32  K33  K34                      K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
@@ -32,4 +31,4 @@ XXX  K20  K21  K22  K23  K24  XXX              XXX          K25  K26  K27  K28  
 
 #endif
 
-#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_CORNE
+#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE
