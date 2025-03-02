acb7491e1dfbb856b4cfdc230ad7f88c6cea21e8
zambo <henrique.zambonin@gmail.com>
2025-03-01 22:48:07 -0300

    fix: typo

--- miryoku/mapping/42/eyelash_corne.h
+++ miryoku/mapping/42/eyelash_corne.h
@@ -1,11 +1,11 @@
 // Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#if !defined (MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE)
+#if !defined (MIRYOKU_LAYOUTMAPPING_CORNE)
 
 #define XXX &none
 
-#define MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE( \
+#define MIRYOKU_LAYOUTMAPPING_CORNE( \
      K00, K01, K02, K03, K04,                               K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,                               K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,                               K25, K26, K27, K28, K29, \
@@ -13,7 +13,7 @@
 ) \
 XXX  K00  K01  K02  K03  K04                   XXX          K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14              XXX  XXX  XXX     K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24  XXX              XXX          K25  K26  K27  K28  K29  XXX \c
+XXX  K20  K21  K22  K23  K24  XXX              XXX          K25  K26  K27  K28  K29  XXX \
                K32  K33  K34                                K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
@@ -67,4 +67,4 @@ XXX  K20  K21  K22  K23  K24  XXX              XXX          K25  K26  K27  K28  
 
 #endif
 
-#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE
+#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_CORNE
