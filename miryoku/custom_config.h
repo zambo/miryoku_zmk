9c1e4943c89a5e9cffca3a19464b013e36582f93
zambo <henrique.zambonin@gmail.com>
2025-03-04 18:54:37 -0300

    feat: experimenting with custom settings/keymaps

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -22,6 +22,18 @@
 #define U_MOUSE_SCROLL_TIME 5000
 #define U_MOUSE_SCROLL_DELAY 0
 
+#define U_BTN1 &mkp MB1
+#define U_BTN2 &mkp MB2
+#define U_BTN3 &mkp MB3
+#define U_MS_D &mmv MOVE_DOWN
+#define U_MS_L &mmv MOVE_LEFT
+#define U_MS_R &mmv MOVE_RIGHT
+#define U_MS_U &mmv MOVE_UP
+#define U_WH_D &msc SCRL_DOWN
+#define U_WH_L &msc SCRL_LEFT
+#define U_WH_R &msc SCRL_RIGHT
+#define U_WH_U &msc SCRL_UP
+
 #define CML &mmv_input_listener { input-processors = <&zip_xy_scaler 2 1>; };
 #define CSL &msc_input_listener { input-processors = <&zip_xy_scaler 2 1>; };
 
