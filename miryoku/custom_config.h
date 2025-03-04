b4f7060e8078d9c1625ed4a3c41eedfa3908d8c1
zambo <henrique.zambonin@gmail.com>
2025-03-04 16:35:34 -0300

    feat: experimenting with custom settings/keymaps

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,8 +1,6 @@
 // Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#if !defined (MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE)
-
 #define XXX &none
 
 #define CVU &kp C_VOL_UP
@@ -61,9 +59,5 @@ XXX  K10  K11  K12  K13  K14       CLE  CEN  CRI  K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24  CMU       CDN       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34                      K35  K36  K37 \
                SCROLL_ENCODER
-
-
-
-#endif
 
 #define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE
