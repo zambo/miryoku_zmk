7614daf3bde76238be539eabc2694949753766a6
zambo <henrique.zambonin@gmail.com>
2025-03-04 17:01:31 -0300

    fix: typo

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -57,7 +57,6 @@
 XXX  K00  K01  K02  K03  K04            CUP       K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14       CLE  CEN  CRI  K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24  CMU       CDN       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34                      K35  K36  K37 \
-               SCROLL_ENCODER
+               K32  K33  K34                      K35  K36  K37
 
 #define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE
