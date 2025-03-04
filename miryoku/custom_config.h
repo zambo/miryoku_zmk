779cf46549878ef8ed133c8f46f4a93c2caeed0d
zambo <henrique.zambonin@gmail.com>
2025-03-04 18:14:07 -0300

    feat: experimenting with custom settings/keymaps

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -25,7 +25,7 @@
 #define CML &mmv_input_listener { input-processors = <&zip_xy_scaler 2 1>; };
 #define CSL &msc_input_listener { input-processors = <&zip_xy_scaler 2 1>; };
 
-#define MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE( \
+#define MIRYOKU_LAYOUTMAPPING_CUSTOM_CORNE( \
      K00, K01, K02, K03, K04,                     K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,                     K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,                     K25, K26, K27, K28, K29, \
@@ -59,4 +59,4 @@ MIRYOKU_X(FUN,    "Fun",        sensor-bindings = <&scroll_encoder>; )
 #define U_SYM    8
 #define U_FUN    9
 
-#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE
+#define MCUSTOMMAPPING MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE
