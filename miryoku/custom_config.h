52b305bf2da22f5662741bde411316548b547d5f
zambo <henrique.zambonin@gmail.com>
2025-03-04 19:49:26 -0300

    feat: experimenting with custom settings/keymaps

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -48,6 +48,20 @@ XXX  K10  K11  K12  K13  K14       CLE  CEN  CRI  K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24  CMU       CDN       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34                      K35  K36  K37
 
+#define MIRYOKU_LAYER_MOUSE ( \
+     K00, K01, K02, K03, K04,                     K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                     K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                     K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                     K35, K36, K37, N38, N39 \
+) \
+XXX  K00  K01  K02  K03  K04               U_MS_U          K05  K06  K07  K08  K09  XXX \
+XXX  K10  K11  K12  K13  K14       U_MS_L  U_MS_L  U_MS_L  K15  K16  K17  K18  K19  XXX \
+XXX  K20  K21  K22  K23  K24  CMU          U_MS_L          K25  K26  K27  K28  K29  XXX \
+               K32  K33  K34                               K35  K36  K37
+
+#define MCUSTOMMAPPING MIRYOKU_LAYOUTMAPPING_CUSTOM_CORNE
+#define MCUSTOMMAPPING MIRYOKU_LAYER_MOUSE
+
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base",       sensor-bindings = <&volume_encoder>; ) \
 MIRYOKU_X(EXTRA,  "Extra",      sensor-bindings = <&scroll_encoder>; ) \
@@ -71,4 +85,3 @@ MIRYOKU_X(FUN,    "Fun",        sensor-bindings = <&scroll_encoder>; )
 #define U_SYM    8
 #define U_FUN    9
 
-#define MCUSTOMMAPPING MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE
