74ae63e24cc01ea89a15b23582956469b9a7c5b0
zambo <henrique.zambonin@gmail.com>
2025-03-04 17:25:56 -0300

    fix: typo

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -44,9 +44,12 @@
     #sensor-binding-cells = <0>;  \
     bindings = <&inc_dec_kp C_VOLUME_UP C_VOLUME_DOWN>; \
   }; \
+  code_nav: code_nav { \
+    compatible = "zmk,behavior-sensor-rotate"; \
+    #sensor-binding-cells = <0>;  \
+    bindings = <&inc_dec_kp U_UND U_RDO>; \
+  }; \
 };
-
-#define SCROLL_ENCODER {sensor-bindings = <&scroll_encoder>;}
 
 #define MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE( \
      K00, K01, K02, K03, K04,                     K05, K06, K07, K08, K09, \
@@ -60,3 +63,30 @@ XXX  K20  K21  K22  K23  K24  CMU       CDN       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34                      K35  K36  K37
 
 #define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE
+
+
+#define MIRYOKU_LAYER_LIST \
+MIRYOKU_X(BASE,   "Base",  sensor-bindings = <&volume_encoder>; ) \
+MIRYOKU_X(EXTRA,  "Extra",  sensor-bindings = <&scroll_encoder>; ) \
+MIRYOKU_X(TAP,    "Tap",  sensor-bindings = <&scroll_encoder>; ) \
+MIRYOKU_X(BUTTON, "Button",  sensor-bindings = <&scroll_encoder>; ) \
+MIRYOKU_X(NAV,    "Nav",  sensor-bindings = <&scroll_encoder>; ) \
+MIRYOKU_X(MOUSE,  "Mouse",  sensor-bindings = <&code_nav>; , ) \
+MIRYOKU_X(MEDIA,  "Media",  sensor-bindings = <&rgb_encoder>; ) , \
+MIRYOKU_X(NUM,    "Num",  sensor-bindings = <&scroll_encoder>; ) , \
+MIRYOKU_X(SYM,    "Sym",  sensor-bindings = <&scroll_encoder>; ) , \
+MIRYOKU_X(FUN,    "Fun",  sensor-bindings = <&scroll_encoder>; )
+
+#define U_BASE   0
+#define U_EXTRA  1
+#define U_TAP    2
+#define U_BUTTON 3
+#define U_NAV    4
+#define U_MOUSE  5
+#define U_MEDIA  6
+#define U_NUM    7
+#define U_SYM    8
+#define U_FUN    9
+
+
+// MIRYOKU_X(BASE,   "Base",   sensor-bindings = < &inc_dec_kp DOWN UP &inc_dec_kp RIGHT LEFT> ;) \
