18db9b01084bce0f946dd5f7cf3df735f95bc1d0
zambo <henrique.zambonin@gmail.com>
2025-03-09 20:39:22 -0300

    feat: testing map customization

---
+++ miryoku/old_custom_config.h
@@ -0,0 +1,128 @@
+// Copyright 2022 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+
+// #define STD &studio_unlock
+
+#define ZMK_MOUSE_DEFAULT_MOVE_VAL 1250
+#define ZMK_MOUSE_DEFAULT_SCRL_VAL 100
+
+#define U_MOUSE_MOVE_EXPONENT 1
+#define U_MOUSE_MOVE_TIME 1500
+#define U_MOUSE_MOVE_DELAY 0
+#define U_MOUSE_SCROLL_EXPONENT 1
+#define U_MOUSE_SCROLL_TIME 5000
+#define U_MOUSE_SCROLL_DELAY 0
+
+#define XXX &none
+
+#define U_BTN1 &mkp MB1
+#define U_BTN2 &mkp MB2
+#define U_BTN3 &mkp MB3
+#define U_MS_D &mmv MOVE_DOWN
+#define U_MS_L &mmv MOVE_LEFT
+#define U_MS_R &mmv MOVE_RIGHT
+#define U_MS_U &mmv MOVE_UP
+#define U_WH_D &msc SCRL_DOWN
+#define U_WH_L &msc SCRL_LEFT
+#define U_WH_R &msc SCRL_RIGHT
+#define U_WH_U &msc SCRL_UP
+
+#define U_VUP       &kp C_VOL_UP
+#define U_VDN       &kp C_VOLUME_DOWN
+#define U_VMT       &kp C_MUTE
+#define U_DR_E      &kp ENTER
+#define U_DR_D      &kp DOWN
+#define U_DR_L      &kp LEFT
+#define U_DR_R      &kp RIGHT
+#define U_DR_U      &kp UP
+
+#define U_ESC         &kp ESC
+#define U_RET         &kp RET
+#define U_BPC         &kp BSPC
+#define U_DEL         &kp DEL
+#define U_SPC         &kp SPACE
+#define U_TAB         &kp TAB
+#define U_LSH         &kp LSHFT
+#define U_LCT         &kp LCTRL
+#define U_LAL         &kp LALT
+#define U_LCM         &kp LGUI
+
+#define TLG         &to U_EXTRA
+#define TLB         &to U_BASE
+
+#define U_RDO &kp LS(LG(Z))
+#define U_PST &kp LG(V)
+#define U_CPY &kp LG(C)
+#define U_CUT &kp LG(X)
+#define U_UND &kp LG(Z)
+
+#define MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE( \
+     K00, K01, K02, K03, K04,                            K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                            K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                            K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                            K35, K36, K37, N38, N39 \
+) \
+ESC  K00  K01  K02  K03  K04              XXX            K05  K06  K07  K08  K09  XXX \
+TAB  K10  K11  K12  K13  K14         XXX  XXX  XXX       K15  K16  K17  K18  K19  TLG \
+LSH  K20  K21  K22  K23  K24   XXX        XXX            K25  K26  K27  K28  K29  TLB \
+               K32  K33  K34                             K35  K36  K37
+
+#define MIRYOKU_LAYERMAPPING_BASE( \
+     K00, K01, K02, K03, K04,                            K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                            K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                            K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                            K35, K36, K37, N38, N39 \
+) \
+ESC  K00  K01  K02  K03  K04            U_DR_U          K05  K06  K07  K08  K09  XXX \
+TAB  K10  K11  K12  K13  K14    U_DR_L  U_DR_E  U_DR_R  K15  K16  K17  K18  K19  TLG \
+LSH  K20  K21  K22  K23  K24  U_VMT     U_DR_D          K25  K26  K27  K28  K29  TLB \
+               K32  K33  K34                            K35  K36  K37
+
+#define MIRYOKU_LAYERMAPPING_MOUSE( \
+     K00, K01, K02, K03, K04,                           K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                           K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                           K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                           K35, K36, K37, N38, N39 \
+) \
+ESC  K00  K01  K02  K03  K04            U_MS_U          K05  K06  K07  K08  K09  XXX \
+TAB  K10  K11  K12  K13  K14    U_MS_L  U_BTN1  U_MS_R  K15  K16  K17  K18  K19  TLG \
+LSH  K20  K21  K22  K23  K24  XXX       U_MS_D          K25  K26  K27  K28  K29  TLB \
+               K32  K33  K34                            K35  K36  K37
+
+// #define MIRYOKU_LAYERMAPPING_GAME( \
+// K00, K01, K02, K03, K04,                           K05, K06, K07, K08, K09, \
+// K10, K11, K12, K13, K14,                           K15, K16, K17, K18, K19, \
+// K20, K21, K22, K23, K24,                           K25, K26, K27, K28, K29, \
+// N30, N31, K32, K33, K34,                           K35, K36, K37, N38, N39 \
+// ) \
+// ESC  K00  K01  K02  K03  K04            U_MS_U          K05  K06  K07  K08  K09  XXX \
+// TAB  K10  K11  K12  K13  K14    U_MS_L  U_BTN1  U_MS_R  K15  K16  K17  K18  K19  XXX \
+// LSH  K20  K21  K22  K23  K24  XXX       U_MS_D          K25  K26  K27  K28  K29  TLB \
+//                K32  K33  K34                            K35  K36  K37
+
+#define MIRYOKU_LAYER_LIST \
+MIRYOKU_X(BASE,   "Base",       sensor-bindings = <&volume_encoder>; ) \
+MIRYOKU_X(EXTRA,  "Extra",      sensor-bindings = <&volume_encoder>; ) \
+MIRYOKU_X(TAP,    "Tap",        sensor-bindings = <&volume_encoder>; ) \
+MIRYOKU_X(BUTTON, "Button",     sensor-bindings = <&rgb_encoder>; ) \
+MIRYOKU_X(NAV,    "Nav",        sensor-bindings = <&volume_encoder>; ) \
+MIRYOKU_X(MOUSE,  "Mouse",      sensor-bindings = <&scroll_encoder>; ) \
+MIRYOKU_X(MEDIA,  "Media",      sensor-bindings = <&volume_encoder>; ) \
+MIRYOKU_X(NUM,    "Num",        sensor-bindings = <&left_right_encoder>; ) \
+MIRYOKU_X(SYM,    "Sym",        sensor-bindings = <&undo_redo_encoder>; ) \
+MIRYOKU_X(FUN,    "Fun",        sensor-bindings = <&up_down_encoder>; )
+// MIRYOKU_X(GAME,   "Game",       sensor-bindings = <&up_down_encoder>; )
+
+#define U_BASE   0
+#define U_EXTRA  1
+#define U_TAP    2
+#define U_BUTTON 3
+#define U_NAV    4
+#define U_MOUSE  5
+#define U_MEDIA  6
+#define U_NUM    7
+#define U_SYM    8
+#define U_FUN    9
+// #define U_GAME  10
+
