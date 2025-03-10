b372c75e8a2b70031d601dd7aafd9334586e29bf
zambo <henrique.zambonin@gmail.com>
2025-03-09 21:27:16 -0300

    feat: added more custom keys to layers

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -57,26 +57,48 @@
 #define U_WH_U &msc SCRL_UP
 
 #define MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE( \
-      K00, K01, K02, K03, K04,                     K05, K06, K07, K08, K09, \
-      K10, K11, K12, K13, K14,                     K15, K16, K17, K18, K19, \
-      K20, K21, K22, K23, K24,                     K25, K26, K27, K28, K29, \
-      N30, N31, K32, K33, K34,                     K35, K36, K37, N38, N39 \
-) \
-XXX   K00  K01  K02  K03  K04            U_MS_U          K05  K06  K07  K08  K09  XXX \
-XXX   K10  K11  K12  K13  K14    U_MS_L  U_BTN1  U_MS_R  K15  K16  K17  K18  K19  XXX \
-XXX   K20  K21  K22  K23  K24  U_VMT     U_MS_D          K25  K26  K27  K28  K29  XXX \
-                K32  K33  K34                            K35  K36  K37
-
-#define MIRYOKU_LAYERMAPPING_BASE( \
-      K00, K01, K02, K03, K04,                            K05, K06, K07, K08, K09, \
-      K10, K11, K12, K13, K14,                            K15, K16, K17, K18, K19, \
-      K20, K21, K22, K23, K24,                            K25, K26, K27, K28, K29, \
-      N30, N31, K32, K33, K34,                            K35, K36, K37, N38, N39 \
+        K00, K01, K02, K03, K04,                           K05, K06, K07, K08, K09, \
+        K10, K11, K12, K13, K14,                           K15, K16, K17, K18, K19, \
+        K20, K21, K22, K23, K24,                           K25, K26, K27, K28, K29, \
+        N30, N31, K32, K33, K34,                           K35, K36, K37, N38, N39 \
 ) \
 U_ESC   K00  K01  K02  K03  K04            U_DR_U          K05  K06  K07  K08  K09  XXX \
+U_TAB   K10  K11  K12  K13  K14    U_DR_L  U_DR_E  U_DR_R  K15  K16  K17  K18  K19  XXX \
+U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D          K25  K26  K27  K28  K29  XXX \
+                  K32  K33  K34                            K35  K36  K37
+
+#define MIRYOKU_LAYERMAPPING_BASE( \
+        K00, K01, K02, K03, K04,                            K05, K06, K07, K08, K09, \
+        K10, K11, K12, K13, K14,                            K15, K16, K17, K18, K19, \
+        K20, K21, K22, K23, K24,                            K25, K26, K27, K28, K29, \
+        N30, N31, K32, K33, K34,                            K35, K36, K37, N38, N39 \
+) \
+U_ESC   K00  K01  K02  K03  K04            U_DR_U          K05  K06  K07  K08  K09  U_BPC \
 U_TAB   K10  K11  K12  K13  K14    U_DR_L  U_DR_E  U_DR_R  K15  K16  K17  K18  K19  U_LG \
 U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D          K25  K26  K27  K28  K29  U_LB \
                   K32  K33  K34                            K35  K36  K37
+
+#define MIRYOKU_LAYERMAPPING_MOUSE( \
+        K00, K01, K02, K03, K04,                            K05, K06, K07, K08, K09, \
+        K10, K11, K12, K13, K14,                            K15, K16, K17, K18, K19, \
+        K20, K21, K22, K23, K24,                            K25, K26, K27, K28, K29, \
+        N30, N31, K32, K33, K34,                            K35, K36, K37, N38, N39 \
+) \
+U_ESC   K00  K01  K02  K03  K04            U_MS_U          K05  K06  K07  K08  K09  U_BPC \
+U_TAB   K10  K11  K12  K13  K14    U_MS_L  U_BTN1  U_MS_R  K15  K16  K17  K18  K19  U_LG \
+U_LSH   K20  K21  K22  K23  K24  U_VMT     U_MS_D          K25  K26  K27  K28  K29  U_LB \
+                  K32  K33  K34                            K35  K36  K37
+
+#define MIRYOKU_LAYERMAPPING_NAV( \
+        K00, K01, K02, K03, K04,                            K05, K06, K07, K08, K09, \
+        K10, K11, K12, K13, K14,                            K15, K16, K17, K18, K19, \
+        K20, K21, K22, K23, K24,                            K25, K26, K27, K28, K29, \
+        N30, N31, K32, K33, K34,                            K35, K36, K37, N38, N39 \
+) \
+U_ESC   K00  K01  K02  K03  K04            U_DR_U          K05  K06  K07  K08  K09  U_DEL \
+U_TAB   K10  K11  K12  K13  K14    U_DR_L  U_DR_E  U_DR_R  K15  K16  K17  K18  K19  U_LG \
+U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D          K25  K26  K27  K28  K29  U_LB \
+              K32  K33  K34                            K35  K36  K37
 
 
 #define MIRYOKU_LAYER_LIST \
