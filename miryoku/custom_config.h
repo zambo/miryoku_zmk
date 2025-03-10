7e923f6527150295e81023f55ab77d11f4c68825
zambo <henrique.zambonin@gmail.com>
2025-03-09 22:57:52 -0300

    feat: game layout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -73,9 +73,9 @@ U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D          K25  K26  K27  K28  K
         K20, K21, K22, K23, K24,                            K25, K26, K27, K28, K29, \
         N30, N31, K32, K33, K34,                            K35, K36, K37, N38, N39 \
 ) \
-U_ESC   K00  K01  K02  K03  K04            U_DR_U           K05  K06  K07  K08  K09  U_BPC \
-U_TAB   K10  K11  K12  K13  K14    U_DR_L  U_DR_E  U_DR_R   K15  K16  K17  K18  K19  U_LG \
-U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D           K25  K26  K27  K28  K29  U_LB \
+U_ESC   K00  K01  K02  K03  K04            U_DR_U           K05  K06  K07  K08  K09  U_BPC      \
+U_TAB   K10  K11  K12  K13  K14    U_DR_L  U_DR_E  U_DR_R   K15  K16  K17  K18  K19  XXX        \
+U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D           K25  K26  K27  K28  K29  &to U_GAME \
                   K32  K33  K34                             K35  K36  K37
 
 #define MIRYOKU_LAYERMAPPING_MOUSE( \
@@ -84,9 +84,9 @@ U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D           K25  K26  K27  K28  
         K20, K21, K22, K23, K24,                            K25, K26, K27, K28, K29, \
         N30, N31, K32, K33, K34,                            K35, K36, K37, N38, N39 \
 ) \
-U_ESC   K00  K01  K02  K03  K04            U_MS_U           K05  K06  K07  K08  K09  U_BPC \
-U_TAB   K10  K11  K12  K13  K14    U_MS_L  U_BTN1  U_MS_R   K15  K16  K17  K18  K19  U_LG \
-U_LSH   K20  K21  K22  K23  K24  U_VMT     U_MS_D           K25  K26  K27  K28  K29  U_LB \
+U_ESC   K00  K01  K02  K03  K04            U_MS_U           K05  K06  K07  K08  K09  XXX \
+U_TAB   K10  K11  K12  K13  K14    U_MS_L  U_BTN1  U_MS_R   K15  K16  K17  K18  K19  XXX \
+U_LSH   K20  K21  K22  K23  K24  U_VMT     U_MS_D           K25  K26  K27  K28  K29  XXX \
                   K32  K33  K34                             K35  K36  K37
 
 #define MIRYOKU_LAYERMAPPING_NAV( \
@@ -96,29 +96,29 @@ U_LSH   K20  K21  K22  K23  K24  U_VMT     U_MS_D           K25  K26  K27  K28  
         N30, N31, K32, K33, K34,                            K35, K36, K37, N38, N39 \
 ) \
 U_ESC   K00  K01  K02  K03  K04            U_DR_U           K05  K06  K07  K08  K09  U_DEL \
-U_TAB   K10  K11  K12  K13  K14    U_DR_L  U_DR_E  U_DR_R   K15  K16  K17  K18  K19  U_LG \
-U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D           K25  K26  K27  K28  K29  U_LB \
+U_TAB   K10  K11  K12  K13  K14    U_DR_L  U_DR_E  U_DR_R   K15  K16  K17  K18  K19  XXX \
+U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D           K25  K26  K27  K28  K29  XXX \
                   K32  K33  K34                             K35  K36  K37
 
 
 // For now, it's simply a QWERTY without home row mods, need to experiment before update
 #define MIRYOKU_LAYER_GAME \
-U_ESC,    &kp Q,    &kp W,    &kp E,      &kp R,      &kp T,              U_DR_U,             &kp Y,    &kp U,     &kp I,       &kp O,    &kp P,      U_NP,      \
-U_TAB,    &kp A,    &kp S,    &kp D,      &kp F,      &kp G,    U_DR_L,   U_DR_E,   U_DR_R,   &kp H,    &kp J,     &kp K,       &kp L,    &kp SQT,    U_NP,      \
-U_LSH,    &kp Z,    &kp X,    &kp C,      &kp V,      &kp B,  U_VMT,      U_DR_D,             &kp N,    &kp M,     &kp COMMA,   &kp DOT,  &kp SLASH,  U_LB,      \
+U_ESC,    &kp Q,    &kp W,    &kp E,      &kp R,      &kp T,              U_DR_U,             &kp Y,    &kp U,     &kp I,       &kp O,    &kp P,      U_NU,         \
+U_TAB,    &kp A,    &kp S,    &kp D,      &kp F,      &kp G,    U_DR_L,   U_DR_E,   U_DR_R,   &kp H,    &kp J,     &kp K,       &kp L,    &kp SQT,    U_NU,         \
+U_LSH,    &kp Z,    &kp X,    &kp C,      &kp V,      &kp B,  U_VMT,      U_DR_D,             &kp N,    &kp M,     &kp COMMA,   &kp DOT,  &kp SLASH,  &to U_BASE,   \
           U_NP,     U_NP,     U_LCT,      U_LAL,      U_LCM,                                  U_NU,     U_NU,      U_NU,        U_NP,     U_NP
 
 
 #define MIRYOKU_LAYERMAPPING_GAME( \
-        K00, K01, K02, K03, K04,                            K05, K06, K07, K08, K09, \
-        K10, K11, K12, K13, K14,                            K15, K16, K17, K18, K19, \
-        K20, K21, K22, K23, K24,                            K25, K26, K27, K28, K29, \
-        N30, N31, K32, K33, K34,                            K35, K36, K37, N38, N39 \
+      K00, K01, K02, K03, K04,                       K05, K06, K07, K08, K09, \
+      K10, K11, K12, K13, K14,                       K15, K16, K17, K18, K19, \
+      K20, K21, K22, K23, K24,                       K25, K26, K27, K28, K29, \
+      N30, N31, K32, K33, K34,                       K35, K36, K37, N38, N39 \
 ) \
-U_ESC   K00  K01  K02  K03  K04            U_DR_U           K05  K06  K07  K08  K09  U_DEL \
-U_TAB   K10  K11  K12  K13  K14    U_DR_L  U_DR_E  U_DR_R   K15  K16  K17  K18  K19  U_LG \
-U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D           K25  K26  K27  K28  K29  U_LB \
-                  K32  K33  K34                             K35  K36  K37
+XXX   K00  K01  K02  K03  K04            XXX         K05  K06  K07  K08  K09  XXX \
+XXX   K10  K11  K12  K13  K14       XXX  XXX  XXX    K15  K16  K17  K18  K19  XXX \
+XXX   K20  K21  K22  K23  K24  XXX       XXX         K25  K26  K27  K28  K29  XXX \
+                K32  K33  K34                        K35  K36  K37
 
 
 #define MIRYOKU_LAYER_LIST \
@@ -131,16 +131,18 @@ MIRYOKU_X(MOUSE,  "Mouse",      sensor-bindings = <&scroll_encoder>; ) \
 MIRYOKU_X(MEDIA,  "Media",      sensor-bindings = <&volume_encoder>; ) \
 MIRYOKU_X(NUM,    "Num",        sensor-bindings = <&left_right_encoder>; ) \
 MIRYOKU_X(SYM,    "Sym",        sensor-bindings = <&undo_redo_encoder>; ) \
-MIRYOKU_X(FUN,    "Fun",        sensor-bindings = <&up_down_encoder>; )
+MIRYOKU_X(FUN,    "Fun",        sensor-bindings = <&up_down_encoder>; ) \
+MIRYOKU_X(GAME,   "Game",       sensor-bindings = <&up_down_encoder>; )
 
-#define U_BASE   0
-#define U_EXTRA  1
-#define U_TAP    2
-#define U_BUTTON 3
-#define U_NAV    4
-#define U_MOUSE  5
-#define U_MEDIA  6
-#define U_NUM    7
-#define U_SYM    8
-#define U_FUN    9
+#define U_BASE    0
+#define U_EXTRA   1
+#define U_TAP     2
+#define U_BUTTON  3
+#define U_NAV     4
+#define U_MOUSE   5
+#define U_MEDIA   6
+#define U_NUM     7
+#define U_SYM     8
+#define U_FUN     9
+#define U_GAME   10
 
