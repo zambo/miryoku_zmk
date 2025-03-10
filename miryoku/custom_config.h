263a03451b5f388d9c519c356e36484a48aa03d8
zambo <henrique.zambonin@gmail.com>
2025-03-10 01:19:50 -0300

    feat: update layouts

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -56,6 +56,14 @@
 #define U_WH_R        &msc SCRL_RIGHT
 #define U_WH_U        &msc SCRL_UP
 
+// MacOS Window Management
+#define U_AFW         &kp LG(TAB)         // Tab Forward
+#define U_ABW         &kp LG(LS(TAB))     //  Tab Backward
+#define U_WFW         &kp LG(GRAVE)       //  Current App Window Forward
+#define U_WBW         &kp LG(LS(GRAVE))   //  Current App Window Backward
+#define U_TFW         &kp LC(TAB)         //  Current App Tab Forward
+#define U_BFW         &kp LC(LS(TAB))     //  Current App Tab Backward
+
 #define MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE( \
         K00, K01, K02, K03, K04,                           K05, K06, K07, K08, K09, \
         K10, K11, K12, K13, K14,                           K15, K16, K17, K18, K19, \
@@ -84,16 +92,29 @@ U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D           K25  K26  K27  K28  
         K20, K21, K22, K23, K24,                            K25, K26, K27, K28, K29, \
         N30, N31, K32, K33, K34,                            K35, K36, K37, N38, N39 \
 ) \
-U_ESC   K00  K01  K02  K03  K04            U_MS_U           K05  K06  K07  K08  K09  XXX \
-U_TAB   K10  K11  K12  K13  K14    U_MS_L  U_BTN1  U_MS_R   K15  K16  K17  K18  K19  XXX \
+U_ABW   K00  K01  K02  K03  K04            U_MS_U           K05  K06  K07  K08  K09  XXX \
+U_WBW   K10  K11  K12  K13  K14    U_MS_L  U_BTN1  U_MS_R   K15  K16  K17  K18  K19  XXX \
 U_LSH   K20  K21  K22  K23  K24  U_VMT     U_MS_D           K25  K26  K27  K28  K29  XXX \
                   K32  K33  K34                             K35  K36  K37
+
 
 #define MIRYOKU_LAYERMAPPING_NAV( \
         K00, K01, K02, K03, K04,                            K05, K06, K07, K08, K09, \
         K10, K11, K12, K13, K14,                            K15, K16, K17, K18, K19, \
         K20, K21, K22, K23, K24,                            K25, K26, K27, K28, K29, \
         N30, N31, K32, K33, K34,                            K35, K36, K37, N38, N39 \
+) \
+U_AFW   K00  K01  K02  K03  K04            U_DR_U           K05  K06  K07  K08  K09  U_DEL \
+U_WFW   K10  K11  K12  K13  K14    U_DR_L  U_DR_E  U_DR_R   K15  K16  K17  K18  K19  XXX \
+U_TFW   K20  K21  K22  K23  K24  U_VMT     U_DR_D           K25  K26  K27  K28  K29  XXX \
+                  K32  K33  K34                             K35  K36  K37
+
+
+#define MIRYOKU_LAYERMAPPING_SYM( \
+      K00, K01, K02, K03, K04,                              K05, K06, K07, K08, K09, \
+      K10, K11, K12, K13, K14,                              K15, K16, K17, K18, K19, \
+      K20, K21, K22, K23, K24,                              K25, K26, K27, K28, K29, \
+      N30, N31, K32, K33, K34,                              K35, K36, K37, N38, N39 \
 ) \
 U_ESC   K00  K01  K02  K03  K04            U_DR_U           K05  K06  K07  K08  K09  U_DEL \
 U_TAB   K10  K11  K12  K13  K14    U_DR_L  U_DR_E  U_DR_R   K15  K16  K17  K18  K19  XXX \
