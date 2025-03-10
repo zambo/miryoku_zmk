a78b04eedef344b597ea187dbed5e16c102728bd
zambo <henrique.zambonin@gmail.com>
2025-03-09 23:15:55 -0300

    feat: update game layout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -106,7 +106,7 @@ U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D           K25  K26  K27  K28  
 &kp Q,    &kp W,    &kp E,      &kp R,      &kp T,      &kp Y,    &kp U,     &kp I,       &kp O,    &kp P,      \
 &kp A,    &kp S,    &kp D,      &kp F,      &kp G,      &kp H,    &kp J,     &kp K,       &kp L,    &kp SQT,    \
 &kp Z,    &kp X,    &kp C,      &kp V,      &kp B,      &kp N,    &kp M,     &kp COMMA,   &kp DOT,  &kp SLASH,  \
-U_NP,     U_NP,     U_LCT,      U_LAL,      U_LCM,      U_NU,     U_NU,      U_NU,        U_NP,     U_NP
+U_NP,     U_NP,     U_LAL,      U_LCM,      U_SPC,      U_NU,     U_NU,      U_NU,        U_NP,     U_NP
 
 
 #define MIRYOKU_LAYERMAPPING_GAME( \
@@ -115,9 +115,9 @@ U_NP,     U_NP,     U_LCT,      U_LAL,      U_LCM,      U_NU,     U_NU,      U_N
       K20, K21, K22, K23, K24,                              K25, K26, K27, K28, K29, \
       N30, N31, K32, K33, K34,                              K35, K36, K37, N38, N39 \
 ) \
-U_ESC   K00  K01  K02  K03  K04            U_DR_U           K05  K06  K07  K08  K09  XXX \
-U_TAB   K10  K11  K12  K13  K14    U_DR_L  U_DR_E  U_DR_R   K15  K16  K17  K18  K19  XXX \
-U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D           K25  K26  K27  K28  K29  &to U_BASE \
+U_TAB   K00  K01  K02  K03  K04            U_DR_U           K05  K06  K07  K08  K09  XXX \
+U_LSH   K10  K11  K12  K13  K14    U_DR_L  U_DR_E  U_DR_R   K15  K16  K17  K18  K19  XXX \
+U_LCT   K20  K21  K22  K23  K24  U_VMT     U_DR_D           K25  K26  K27  K28  K29  &to U_BASE \
                   K32  K33  K34                             K35  K36  K37
 
 
