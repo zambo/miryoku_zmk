715cc36f4865beece2f20baef5b2fee3016ea4ef
zambo <henrique.zambonin@gmail.com>
2025-03-09 23:05:53 -0300

    fix: game layout map

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -103,22 +103,22 @@ U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D           K25  K26  K27  K28  
 
 // For now, it's simply a QWERTY without home row mods, need to experiment before update
 #define MIRYOKU_LAYER_GAME \
-U_ESC,    &kp Q,    &kp W,    &kp E,      &kp R,      &kp T,              U_DR_U,             &kp Y,    &kp U,     &kp I,       &kp O,    &kp P,      U_NU,         \
-U_TAB,    &kp A,    &kp S,    &kp D,      &kp F,      &kp G,    U_DR_L,   U_DR_E,   U_DR_R,   &kp H,    &kp J,     &kp K,       &kp L,    &kp SQT,    U_NU,         \
-U_LSH,    &kp Z,    &kp X,    &kp C,      &kp V,      &kp B,  U_VMT,      U_DR_D,             &kp N,    &kp M,     &kp COMMA,   &kp DOT,  &kp SLASH,  &to U_BASE,   \
-          U_NP,     U_NP,     U_LCT,      U_LAL,      U_LCM,                                  U_NU,     U_NU,      U_NU,        U_NP,     U_NP
+&kp Q,    &kp W,    &kp E,      &kp R,      &kp T,      &kp Y,    &kp U,     &kp I,       &kp O,    &kp P,      \
+&kp A,    &kp S,    &kp D,      &kp F,      &kp G,      &kp H,    &kp J,     &kp K,       &kp L,    &kp SQT,    \
+&kp Z,    &kp X,    &kp C,      &kp V,      &kp B,      &kp N,    &kp M,     &kp COMMA,   &kp DOT,  &kp SLASH,  \
+U_NP,     U_NP,     U_LCT,      U_LAL,      U_LCM,      U_NU,     U_NU,      U_NU,        U_NP,     U_NP
 
 
 #define MIRYOKU_LAYERMAPPING_GAME( \
-      K00, K01, K02, K03, K04,                       K05, K06, K07, K08, K09, \
-      K10, K11, K12, K13, K14,                       K15, K16, K17, K18, K19, \
-      K20, K21, K22, K23, K24,                       K25, K26, K27, K28, K29, \
-      N30, N31, K32, K33, K34,                       K35, K36, K37, N38, N39 \
+      K00, K01, K02, K03, K04,                              K05, K06, K07, K08, K09, \
+      K10, K11, K12, K13, K14,                              K15, K16, K17, K18, K19, \
+      K20, K21, K22, K23, K24,                              K25, K26, K27, K28, K29, \
+      N30, N31, K32, K33, K34,                              K35, K36, K37, N38, N39 \
 ) \
-XXX   K00  K01  K02  K03  K04            XXX         K05  K06  K07  K08  K09  XXX \
-XXX   K10  K11  K12  K13  K14       XXX  XXX  XXX    K15  K16  K17  K18  K19  XXX \
-XXX   K20  K21  K22  K23  K24  XXX       XXX         K25  K26  K27  K28  K29  XXX \
-                K32  K33  K34                        K35  K36  K37
+U_ESC   K00  K01  K02  K03  K04            U_DR_U           K05  K06  K07  K08  K09  XXX \
+U_TAB   K10  K11  K12  K13  K14    U_DR_L  U_DR_E  U_DR_R   K15  K16  K17  K18  K19  XXX \
+U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D           K25  K26  K27  K28  K29  &to U_BASE \
+                  K32  K33  K34                             K35  K36  K37
 
 
 #define MIRYOKU_LAYER_LIST \
