929b236f4cf1da525590eea380476678d3e4312b
zambo <henrique.zambonin@gmail.com>
2025-03-12 07:19:41 -0300

    feat: include the "Grave Escape" behavior

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -65,16 +65,16 @@
 #define U_BFW         &kp LC(LS(TAB))     //  Current App Tab Backward
 
 // MOD MORPH
-#define U_GSC         &gresc
+#define U_GSC         &grvsc
 #define U_BKDL        &bspc_del
 
 // Updating the base to include the "Grave Escape" behavior
 // https://zmk.dev/docs/keymaps/behaviors/mod-morph#mod-morph
 #define MIRYOKU_LAYER_BASE \
-&kp Q,             &kp W,             &kp F,                &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),       U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,                &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, &gresc),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+&kp Q,             &kp W,             &kp F,                &kp P,             &kp B,             &kp J,             &kp L,               &kp U,             &kp Y,             &kp SQT,           \
+U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),       U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),      U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,                &kp D,             &kp V,             &kp K,             &kp H,               &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, U_GSC), U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, U_BKDL), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE( \
   K00, K01, K02, K03, K04,                           K05, K06, K07, K08, K09, \
