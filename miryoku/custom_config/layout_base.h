cc35fb5c89386a59d2cb1748a3863b99e01d8130
zambo <henrique.zambonin@gmail.com>
2025-03-19 21:53:30 -0300

    feat: add layer change keys to base layer

--- miryoku/custom_config/layout_base.h
+++ miryoku/custom_config/layout_base.h
@@ -12,7 +12,7 @@ U_NP,                   U_NP,             U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),
      K20, K21, K22, K23, K24,                            K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                            K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04            U_MS_U           K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14    U_MS_L  U_BTN1  U_MS_R   K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24  U_VMT     U_MS_D           K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34                             K35  K36  K37
+U_LT(U_FUN, U_ESC)  K00  K01  K02  K03  K04            U_MS_U           K05  K06  K07  K08  K09  U_LT(U_MEDIA, U_BPC) \
+U_LT(U_NUM, U_TAB)  K10  K11  K12  K13  K14    U_MS_L  U_BTN1  U_MS_R   K15  K16  K17  K18  K19  U_LT(U_NAV, XXX) \
+U_LT(U_SYM, U_LSH)  K20  K21  K22  K23  K24  U_VMT     U_MS_D           K25  K26  K27  K28  K29  U_LT(U_MOUSE, XXX) \
+                    K32  K33  K34                                       K35  K36  K37
