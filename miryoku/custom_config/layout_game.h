3fa021dc2f3d58bea7202cef1bbad13218cda542
zambo <henrique.zambonin@gmail.com>
2025-03-19 22:39:32 -0300

    fix: sup key

--- miryoku/custom_config/layout_game.h
+++ miryoku/custom_config/layout_game.h
@@ -15,4 +15,3 @@ U_ESC   K00  K01  K02  K03  K04            U_DR_U           K05  K06  K07  K08  
 U_TAB   K10  K11  K12  K13  K14    U_DR_L  U_DR_E  U_DR_R   K15  K16  K17  K18  K19  XXX \
 U_LSH   K20  K21  K22  K23  K24  U_VMT     U_DR_D           K25  K26  K27  K28  K29  &to U_BASE \
                   K32  K33  K34                             K35  K36  K37
-
